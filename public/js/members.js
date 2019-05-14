70cbc0ea5753253e2549c63eb49d65fc63fe864c
Your Name <tundun.deborah@gmail.com>
2019-05-14 00:16:21 -0400

    "added user name and links to members file"

--- public/js/members.js
+++ public/js/members.js
@@ -1,46 +1,9 @@
-$(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function(data) {
-      $(".member-name").text(data.name);
-      //$(".member-name").text(data.email);
-     
-    });
-  });
+$(document).ready(function () {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function (data) {
+    $(".member-name").text(data.name);
+    //console.log(data)
 
-  $(function () {
-    $(".completed-button").on("click", function (event) {
-      var id = $(this).data("id");
-      var newlyCompleted = "true";
-  
-      var newlyCompletedState = {
-        completed: newlyCompleted
-      };
-  
-      $.ajax("/api/Tasks/" + id, {
-        type: "PUT",
-        data: newlyCompletedState
-      }).then(function () {
-        location.reload();
-      }
-      );
-    });
-  
-    $(".create-form").on("submit", function (event) {
-      event.preventDefault();
-  
-      var newTask = {
-        name: $("#Task").val().trim(),
-      };
-  
-      $.ajax("/api/Tasks", {
-        type: "POST",
-        data: newTask
-      }).then(
-        function () {
-          location.reload();
-        }
-      );
-    });
-  
-  })
+  });
+});
